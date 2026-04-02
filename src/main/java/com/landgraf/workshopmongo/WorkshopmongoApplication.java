023f382a728510abfaa7ce452ae8eab50bb84ac7
Thomas Landgraf <thomas.ml@hotmail.com>
2026-04-02 10:45:36 -0700

    Instantiation class

--- src/main/java/com/landgraf/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/landgraf/workshopmongo/WorkshopmongoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 
-@SpringBootApplication // Deixe apenas assim, sem o (exclude = ...)
+@SpringBootApplication
 public class WorkshopmongoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
