6a13bc715b81a1db0f232933d067a4340f70f8ec
Thomas Landgraf <thomas.ml@hotmail.com>
2026-04-02 11:40:04 -0700

    User by Id

--- src/main/java/com/landgraf/workshopmongo/resources/UserResource.java
+++ src/main/java/com/landgraf/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.landgraf.workshopmongo.dto.UserDTO;
 import com.landgraf.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +28,9 @@ public class UserResource {
         return ResponseEntity.ok().body(userDTOList);
     }
 
+    @RequestMapping(value= "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
 }
--- src/main/java/com/landgraf/workshopmongo/services/UserService.java
+++ src/main/java/com/landgraf/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.landgraf.workshopmongo.services;
 
 import com.landgraf.workshopmongo.domain.User;
 import com.landgraf.workshopmongo.repository.UserRepository;
+import com.landgraf.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found!"));
     }
 }
