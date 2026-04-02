9bb8c798f8b48265f56265c93442830412d2a0da
Thomas Landgraf <thomas.ml@hotmail.com>
2026-04-02 10:56:14 -0700

    UserDTO

--- src/main/java/com/landgraf/workshopmongo/resources/UserResource.java
+++ src/main/java/com/landgraf/workshopmongo/resources/UserResource.java
@@ -1,18 +1,17 @@
 package com.landgraf.workshopmongo.resources;
 
 import com.landgraf.workshopmongo.domain.User;
-import com.landgraf.workshopmongo.repository.UserRepository;
+import com.landgraf.workshopmongo.dto.UserDTO;
 import com.landgraf.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,14 +20,11 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = userService.findAll();
-        System.out.println("Tamanho da lista: " + list.size());
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> userDTOList = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
     }
 
 }
