c1ea16b92bc729867173572e82b21a0bb7c8b197
Thomas Landgraf <thomas.ml@hotmail.com>
2026-04-03 12:05:18 -0700

    Revert "Querrys with operations"
    This reverts commit e242ce524a30baa68b7bfd3fd44050b81d612515.

--- src/main/java/com/landgraf/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/landgraf/workshopmongo/repository/PostRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -15,7 +14,4 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> findByTitle(String text);
-
-    @Query("{ $and: [ {date: {$gte: ?1} }, {date: {$lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
-    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/landgraf/workshopmongo/services/PostService.java
+++ src/main/java/com/landgraf/workshopmongo/services/PostService.java
@@ -6,7 +6,6 @@ import com.landgraf.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,10 +22,5 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return postRepository.findByTitle(text);
-    }
-
-    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
-        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
-        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
